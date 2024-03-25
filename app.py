f62fa2e5443125aa55ede747002f5cbefd968e2b
Gabriel Araujo <gabrielsant95@gmail.com>
2024-03-24 22:17:37 -0300

    Refactoring performed on the project as a whole - new features and OO structure

--- app.py
+++ app.py
@@ -1,125 +1,21 @@
-import os
-
-clientes = [{'nome': 'Gabriella Izabel', 'categoria': 'Premium', 'ativo': True},
-            {'nome': 'Andre Santana', 'categoria': 'Platinum', 'ativo': False},
-            {'nome': 'Rosane Nô', 'categoria': 'Gold', 'ativo': True},
-            {'nome': 'Gabriel Araujo', 'categoria': 'Basic', 'ativo': True}]
+from modelos.cliente import Cliente
+from modelos.produto import Produtos
 
 
-def exibir_nome_do_programa():
-    print('𝓜𝓪𝓻𝓲𝓪 𝓡𝓸𝓼𝓪\n')
+conjunto_body_01 = Produtos('Body Cachorrinho', '39.99', 'branco e bege', 'M')
+
+cliente_1 = Cliente('Gabriel', 'Gold')
+cliente_2 = Cliente('Andre', 'Platinum')
+cliente_3 = Cliente('Rosane', 'Silver')
 
 
-def listar_opcoes():
-    print("1. Cadastrar cliente")
-    print("2. Listar cliente")
-    print("3. Alterar status do cliente")
-    print("4. Sair\n")
-
-
-def terminate_app():
-    exibir_subtitulo('Finalizando a aplicação')
-
-
-def voltar_ao_menu():
-    input('\nChoose an option to get back to main menu.')
-    main()
-
-
-def input_invalido():
-    print('Invalid option\n')
-    voltar_ao_menu()
-
-
-def exibir_subtitulo(text):
-    os.system('cls')
-    linha = '*' * (len(text) + 4)
-    print(linha)
-    print(text)
-    print(linha)
-    print()
-
-
-def add_novo_cliente():
-    '''Função que adiciona novo cliente a base de dados.
-
-    Inputs:
-    - Nome do Cliente
-    - Categoria do Cliente(plano escolhido)
-
-    Output:
-    - Adiciona novo cliente a base de clientes em memória.
-
-    '''
-    exibir_subtitulo("Cadastro de novos clientes")
-    nome_cliente = input("Digite o nome do cliente que deseja cadastrar: ")
-    categoria = input('Digite o tipo de membership desejada: ')
-    dados_do_cliente = {'nome': nome_cliente,
-                        'categoria': categoria,
-                        'ativo': False}
-    clientes.append(dados_do_cliente)
-    clientes.append(nome_cliente)
-    print(f"Cliente {nome_cliente} foi cadastrado com sucesso. ")
-    voltar_ao_menu()
-
-
-def listar_clientes_cadastrados():
-    exibir_subtitulo('Listing clients: ')
-
-    print(f'{'Nome do cliente'.ljust(22)} | {'Categoria'.ljust(20)} | Status')
-
-    for client in clientes:
-        client_name = client['nome']
-        categoria = client['categoria']
-        ativo = 'ativado' if client['ativo'] else 'desativado'
-        print(f' - {client_name.ljust(20)} | {categoria.ljust(20)} | {ativo}')
-    voltar_ao_menu()
-
-
-def alternar_status_do_cliente():
-    exibir_subtitulo('Alterando status do restaurante')
-    nome_cliente = input(
-        'Digite o nome do cliente que deseja alterar o status:')
-    cliente_encontrado = False
-
-    for cliente in clientes:
-        if nome_cliente == cliente['nome']:
-            cliente_encontrado = True
-            cliente['ativo'] = not cliente['ativo']
-            mensagem = f'O {nome_cliente} foi ativado com sucesso.' if cliente['ativo'] else f'O  cliente: {
-                nome_cliente} foi desativado com sucesso.'
-            print(mensagem)
-
-    if not cliente_encontrado:
-        print('Cliente nao encontrado.')
-
-    voltar_ao_menu()
-
-
-def chosen_option():
-    try:
-        user_option = int(input("Escolha uma opção: "))
-        print(f"Opcão escolhida: {user_option}")
-
-        if user_option == 1:
-            add_novo_cliente()
-        elif user_option == 2:
-            listar_clientes_cadastrados()
-        elif user_option == 3:
-            alternar_status_do_cliente()
-        elif user_option == 4:
-            terminate_app()
-        else:
-            input_invalido()
-    except:
-        input_invalido()
+conjunto_body_01.receber_avaliacao(cliente_1._nome, 10)
+conjunto_body_01.receber_avaliacao(cliente_2._nome, 7)
+conjunto_body_01.receber_avaliacao(cliente_3._nome, 6)
 
 
 def main():
-    os.system('cls')
-    exibir_nome_do_programa()
-    listar_opcoes()
-    chosen_option()
+    Cliente.listar_clientes()
 
 
 if __name__ == '__main__':
---
+++ modelos/produto.py
@@ -0,0 +1,39 @@
+from .avaliacao_produto import Avaliacao_Produto
+
+
+class Produtos():
+
+    produtos = []
+
+    def __init__(self, nome, preco, cor, tamanho):
+        self._nome = nome
+        self._preco = preco
+        self._cor = cor
+        self._tamanho = tamanho
+        self._avaliacao_produto = []
+        Produtos.produtos.append(self)
+
+    def __str__(self):
+        return f'{self._nome} | {self._preco}'
+
+    @classmethod
+    def listar_produtos(cls):
+        print(f'{"NOME".ljust(25)} | {
+              'PRECO'.ljust(25)} | {'Avaliacao'.ljust(25)} |{'COR\n'}')
+        for produto in cls.produtos:
+            print(f'{produto._nome.ljust(25)} | {
+                  produto._preco.ljust(25)} | ATIVO {produto._cor}')
+
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao_produto = Avaliacao_Produto(cliente, nota)
+        self._avaliacao_produto.append(avaliacao_produto)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao_produto:
+            return 0
+        soma_das_notas = sum(
+            avaliacao_produto._nota for avaliacao_produto in self._avaliacao_produto)
+        quantidade_de_notas = len(self._avaliacao_produto)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
