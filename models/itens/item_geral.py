24cd08186e94a838077708ce2a068efce2844aa7
Gabriel Araujo <gabrielsant95@gmail.com>
2024-03-30 23:24:26 -0300

    Implementing abstract methods

--- models/itens/item_geral.py
+++ models/itens/item_geral.py
@@ -1,4 +1,11 @@
-class ItemGeral:
+from abc import ABC, abstractmethod
+
+
+class ItemGeral(ABC):
     def __init__(self, nome, preco):
         self._nome = nome
         self._preco = preco
+
+    @abstractmethod
+    def aplicar_desconto(self):
+        pass
